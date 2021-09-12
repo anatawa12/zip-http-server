d45dffc6bc918e12f3cb9623cbb68ed531f8da08
anatawa12 <anatawa12@icloud.com>
2021-09-12 13:06:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zip Http Server
+
+The http server exports contents in zip files.
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Response};
+use simple_logger::SimpleLogger;
+use std::convert::Infallible;
+use std::env::args_os;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::PathBuf;
+use std::sync::Arc;
+use zip::result::ZipError;
+use zip::ZipArchive;
+
+#[tokio::main]
+async fn main() {
+    SimpleLogger::new().init().unwrap();
+
+    log::info!("starting server...");
+
+    let mut args = args_os();
+    args.next(); // executable name
+    let zip_file: PathBuf = args.next().expect("no path to zip file").into();
+    log::info!("using zip {}", &zip_file.display());
+    let zip_file: Arc<PathBuf> = Arc::new(zip_file).into();
+    // And a MakeService to handle each connection...
+    let make_svc = make_service_fn(move |_| {
+        let zip_file = zip_file.clone();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                let zip_file = zip_file.clone();
+                async move {
+                    let res = match handle(zip_file, req.uri().path()).await {
+                        Ok(vec) => Response::builder().body(Body::from(vec)).unwrap(),
+                        Err(HandleError::IO(err)) => {
+                            log::error!("io error {}", err);
+                            internal_server_error()
+                        }
+                        Err(HandleError::Invalid(str)) => {
+                            log::error!("invalid archive {}", str);
+                            internal_server_error()
+                        }
+                        Err(HandleError::NotFound) => {
+                            log::error!("not found: {}", req.uri().path());
+                            not_found()
+                        }
+                    };
+                    Ok::<_, Infallible>(res)
+                }
+            }))
+        }
+    });
+
+    let server = hyper::Server::bind(&([0, 0, 0, 0], 80).into()).serve(make_svc);
+
+    log::info!("server started!");
+    // Run forever-ish...
+    if let Err(err) = server.await {
+        log::error!("server error: {}", err);
+    }
+}
+
+fn internal_server_error() -> Response<Body> {
+    Response::builder()
+        .status(500)
+        .header("Content-Type", "text/plain")
+        .body(Body::from("internal server error"))
+        .unwrap()
+}
+
+fn not_found() -> Response<Body> {
+    Response::builder()
+        .status(404)
+        .header("Content-Type", "text/plain")
+        .body(Body::from("NOT FOUND"))
+        .unwrap()
+}
+
+enum HandleError {
+    IO(std::io::Error),
+    Invalid(&'static str),
+    NotFound,
+}
+
+impl From<std::io::Error> for HandleError {
+    fn from(err: std::io::Error) -> Self {
+        HandleError::IO(err)
+    }
+}
+
+impl From<ZipError> for HandleError {
+    fn from(err: ZipError) -> Self {
+        match err {
+            ZipError::FileNotFound => HandleError::NotFound,
+            ZipError::InvalidArchive(err) => HandleError::Invalid(err),
+            ZipError::UnsupportedArchive(err) => HandleError::Invalid(err),
+            ZipError::Io(err) => HandleError::IO(err),
+        }
+    }
+}
+
+async fn handle(zip_file: Arc<PathBuf>, name: &str) -> Result<Vec<u8>, HandleError> {
+    let name = name.to_owned();
+    match tokio::task::spawn_blocking(move || handle_blocking(zip_file, name)).await {
+        Ok(res) => res,
+        Err(err) => std::panic::resume_unwind(err.into_panic()),
+    }
+}
+
+fn handle_blocking(zip_file: Arc<PathBuf>, mut name: String) -> Result<Vec<u8>, HandleError> {
+    if name.ends_with('/') {
+        name = format!("{}index.html", name);
+    }
+    let name = name.strip_prefix('/').unwrap_or(&name);
+    let zip_file = File::open(&*zip_file)?;
+    let zip_file = BufReader::new(zip_file);
+    let mut zip_file = ZipArchive::new(zip_file)?;
+
+    log::info!("looking for {}", name);
+    let mut file = zip_file.by_name(name)?;
+    let mut vec = Vec::with_capacity(file.size() as usize);
+    file.read_to_end(&mut vec)?;
+
+    Ok(vec)
+}
